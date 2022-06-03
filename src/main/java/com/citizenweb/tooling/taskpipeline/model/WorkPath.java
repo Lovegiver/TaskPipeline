daea1cc4edef16b7d52acf3fd791b2220ec24192
Lovegiver <lovegiver@protonmail.com>
2022-06-03 23:21:07 +0200

    squash! Merge remote-tracking branch 'origin/WorkPath_validation' into WorkPath_validation

--- src/main/java/com/citizenweb/tooling/taskpipeline/model/WorkPath.java
+++ src/main/java/com/citizenweb/tooling/taskpipeline/model/WorkPath.java
@@ -1,13 +1,16 @@
 package com.citizenweb.tooling.taskpipeline.model;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import reactor.core.publisher.Flux;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
 /**
@@ -23,6 +26,7 @@ public class WorkPath {
     @Getter
     private final Task endingTask;
     /** For a given {@link Task}, associated values are the input fluxes needed to execute the {@link Operation#process(Flux[])}. */
+    @Getter
     private final Map<Task, Collection<Flux<?>>> tasksRelationships = new ConcurrentHashMap<>();
 
     public WorkPath(Set<Task> taskToProcess) {
@@ -31,4 +35,17 @@ public class WorkPath {
         this.endingTask = taskToProcess.stream().filter(Task.isTerminalTask).findAny().orElseThrow();
         this.name = taskToProcess.stream().filter(Task.isTerminalTask).map(Task::getTaskName).findAny().orElseThrow();
     }
+
+    /** Each time a {@link Flux} is produced, we have to inject it as an input of the next {@link Task}.<br>
+     * The Task and its inputs are temporarily stored in a local {@link ConcurrentHashMap}.<br> */
+    BiConsumer<Task,Flux<?>> injectFluxIntoNextTask = ((next, flux) -> {
+        if (this.tasksRelationships.get(next) != null) {
+            this.tasksRelationships.get(next).add(flux);
+        } else {
+            Set<Flux<?>> fluxSet = new HashSet<>();
+            fluxSet.add(flux);
+            this.tasksRelationships.put(next, fluxSet);
+        }
+    });
+
 }
