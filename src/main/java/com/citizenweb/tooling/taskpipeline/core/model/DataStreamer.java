f01b5e3a1a532e878e4d4eb77d1526bf898f7571
Lovegiver <lovegiver@proton.me>
2022-07-18 00:27:11 +0200

    Replaced List by CopyOnWriteArray to fix a ConcurrentModificationException in DataStreamer

--- src/main/java/com/citizenweb/tooling/taskpipeline/core/model/DataStreamer.java
+++ src/main/java/com/citizenweb/tooling/taskpipeline/core/model/DataStreamer.java
@@ -13,11 +13,10 @@ import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 /**
@@ -31,9 +30,9 @@ public class DataStreamer {
     /** A {@link ServerSentEvent} has to be named */
     private final String SSE_EVENT_NAME = "Pipeline";
     /** For each {@link Pipeline} we store a collection of {@link ServerSentEvent}s */
-    private final ConcurrentHashMap<Pipeline, List<ServerSentEvent<String>>> notificationsMap = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Pipeline, CopyOnWriteArrayList<ServerSentEvent<String>>> notificationsMap = new ConcurrentHashMap<>();
     /** Stores the unique instance of {@link DataStreamer} */
-    private static final DataStreamer DATA_STREAMER = new DataStreamer();
+    private static final AtomicReference<DataStreamer> DATA_STREAMER = new AtomicReference<>(new DataStreamer());
     /** Converts a {@link Pipeline} into a {@link PipelineDTO} */
     private final Function<Pipeline, PipelineDTO> convertToDTO = PipelineDTO::new;
     /** Converts a {@link PipelineDTO} into a {@link ServerSentEvent} */
@@ -55,11 +54,11 @@ public class DataStreamer {
 
     /** Returns the sole instance of {@link DataStreamer} */
     public static DataStreamer getInstance() {
-        return DATA_STREAMER;
+        return DATA_STREAMER.get();
     }
 
     /** Creates a {@link Flux} based on the Map's content */
-    public Flux<ServerSentEvent<String>> exportData() {
+    public synchronized Flux<ServerSentEvent<String>> exportData() {
         log.info("In Exporter -> Queue size = " + notificationsMap.size());
         return Flux.create(sse -> notificationsMap.values().stream()
                 .flatMap(Collection::stream)
@@ -67,7 +66,7 @@ public class DataStreamer {
     }
 
     /** Creates a {@link Flux} based on Map's content for a given {@link Pipeline} */
-    public Flux<ServerSentEvent<String>> exportData(Pipeline pipeline) {
+    public synchronized Flux<ServerSentEvent<String>> exportData(Pipeline pipeline) {
         log.info("In Exporter -> Queue size = " + notificationsMap.size());
         return Flux.create(sse -> notificationsMap.entrySet().stream()
                 .filter(entry -> pipeline == entry.getKey())
@@ -79,9 +78,9 @@ public class DataStreamer {
     /** Calling this method will snapshot the state of the given {@link Pipeline} */
     public synchronized void triggerNotification(Pipeline pipeline) {
         log.info("In Consumer -> Pipeline = " + pipeline);
-        List<ServerSentEvent<String>> serverSentEvents;
+        CopyOnWriteArrayList<ServerSentEvent<String>> serverSentEvents;
         if (notificationsMap.get(pipeline) == null) {
-            serverSentEvents = new ArrayList<>();
+            serverSentEvents = new CopyOnWriteArrayList<>();
         } else {
             serverSentEvents = notificationsMap.get(pipeline);
         }
