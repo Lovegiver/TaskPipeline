bf5add8a7690eb65247cc1074bd57a58ea1e8523
Lovegiver <lovegiver@proton.me>
2022-07-27 22:15:02 +0200

    Comment

--- src/main/java/com/citizenweb/tooling/taskpipeline/core/model/Notifier.java
+++ src/main/java/com/citizenweb/tooling/taskpipeline/core/model/Notifier.java
@@ -3,6 +3,7 @@ package com.citizenweb.tooling.taskpipeline.core.model;
 /**
  * A {@link Notifier} has a single method  used by any {@link Monitorable} object to trigger the wrapping
  * {@link Pipeline}'s state export.
+ * Its usage is a simple implementation of the <b>Visitor pattern</b>
  */
 @FunctionalInterface
 public interface Notifier {
